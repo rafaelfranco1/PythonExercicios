fa45510d49f56988c1599321d0e9742cd3abcce6
rafaelfranco1 <42389814+rafaelfranco1@users.noreply.github.com>
2022-07-23 10:23:52 -0300

    Add files via upload

---
+++ AntecessoreSucessor.py
@@ -0,0 +1,6 @@
+#Antecessor e Sucessor de um número
+n1 = int(input('Digite um número: '))
+ant = n1 - 1
+sus = n1 + 1
+print ('Analisando o valor {} , seu antecessor é {} e o sucessor é {}'
+.format(n1,ant,sus))
