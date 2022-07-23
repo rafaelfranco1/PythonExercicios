2794ad6b5a1255a0d65879f9cd01e940ceb828c4
rafaelfranco1 <42389814+rafaelfranco1@users.noreply.github.com>
2022-07-23 10:03:14 -0300

    Add files via upload

---
+++ Desafio04 Concatenacao de Strings.py
@@ -0,0 +1,6 @@
+# Exercício de concatenação de Strings
+n1 = int(input('Digite um valor : '))
+n2 = int(input('Digite outro: '))
+s = n1 + n2
+#print('A soma entre ',n1,' e ' , n2, ' é ', s) método aspas e vírgula
+print('A soma entre {} e {} é {}'.format(n1,n2,s)) 
