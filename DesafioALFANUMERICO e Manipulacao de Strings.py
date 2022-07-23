8bbb3cf54ca68b8034dc842ae9075482c42c9675
rafaelfranco1 <42389814+rafaelfranco1@users.noreply.github.com>
2022-07-23 09:55:03 -0300

    Add files via upload

---
+++ DesafioALFANUMERICO e Manipulacao de Strings.py
@@ -0,0 +1,6 @@
+print('Digite uma string para saber seu tipo , se é alfanumérico , numérico mou alfbetico:  ');
+n1 = input('Digite o valor: ')
+print(type(n1))
+print(n1.isnumeric())
+print(n1.isalpha())
+print(n1.isalphanum())
